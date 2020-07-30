547cf28bd9cc3d78b3db57bd86e75d86009a9a7b
nitya1507 <43795797+nitya1507@users.noreply.github.com>
2020-07-30 22:11:01 +0530

    Add files via upload

---
+++ command.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+                    $('.banner').owlCarousel({
+                            loop:true,
+                            margin:0,
+                            nav:false,
+                            autoplay:true,
+                            smartSpeed:2000,
+                            responsive:{
+                                0:{
+                                    items:1
+                                },
+                                600:{
+                                    items:1
+                                },
+                                1000:{
+                                    items:1
+                                }
+                            }
+                        });
+                    $('.fruitlist .owl-carousel').owlCarousel({
+                        loop:true,
+                        margin:10,
+                        nav:true,
+                        responsive:{
+                            0:{
+                                items:1
+                            },
+                            600:{
+                                items:4
+                            },
+                            1000:{
+                                items:4
+                            }
+                        }
+                    });
+                     $('.photo2 .owl-carousel').owlCarousel({
+                        loop:true,
+                        margin:10,
+                        nav:false,
+                        responsive:{
+                            0:{
+                                items:1
+                            },
+                            600:{
+                                items:4
+                            },
+                            1000:{
+                                items:4
+                            }
+                        }
+                    });
+                      $('.blogphoto .owl-carousel').owlCarousel({
+                        loop:false,
+                        margin:10,
+                        nav:false,
+                        responsive:{
+                            0:{
+                                items:1
+                            },
+                            600:{
+                                items:3
+                            },
+                            1000:{
+                                items:3
+                            }
+                        }
+                    });
+                       $('.testi .owl-carousel').owlCarousel({
+                        loop:true,
+                        margin:10,
+                        nav:false,
+                        responsive:{
+                            0:{
+                                items:1
+                            },
+                            600:{
+                                items:1
+                            },
+                            1000:{
+                                items:1
+                            }
+                        }
+                    });
+});
+
